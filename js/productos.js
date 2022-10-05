f314cc32616c63ea0004b03992a1fdd093bd22de
Matias Morini <usermatiasmorini7@gmail.com>
2022-10-04 21:44:52 -0300

    first commit

---
+++ js/productos.js
@@ -0,0 +1,5 @@
+const productos = [
+    {id:01, tipo:"Tarjeta de video", nombre:"Gforce RTX 3070", precio: 250000, foto: "./img/imagen1.png" },
+    {id:02, tipo:"Procesador", nombre:"Intel I7 9700", precio: 120000, foto: "./img/imagen2.png" },
+    {id:03, tipo:"Procesador", nombre:"Ryzen 5300", precio: 80000, foto: "./img/imagen3.png" },
+];
